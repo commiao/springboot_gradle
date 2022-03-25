3972166ea6b9eedbf79aa02e2fbdd4b4af1e8e06
jingmiao <jingmiao@chtwm.com>
2022-03-25 21:44:48 +0800

    添加map与set操作

--- redisson-spring-boot-starter/src/main/java/com/miao/db/redisson/RedisSetHander.java
+++ redisson-spring-boot-starter/src/main/java/com/miao/db/redisson/RedisSetHander.java
@@ -1,13 +1,18 @@
 package com.miao.db.redisson;
 
+import org.redisson.api.RFuture;
 import org.redisson.api.RSet;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 不排序集合
+ */
 @Component
 public class RedisSetHander {
     @Autowired
@@ -18,9 +23,17 @@ public class RedisSetHander {
         return rSet.readAll();
     }
 
+    //追加数据
     public <T> boolean add(String key, T bean) {
         RSet<T> rSet = redissonClient.getSet(key);
         return rSet.add(bean);
+    }
+
+    //异步追加数据
+    public <T> Boolean addAsync(String key, T bean) throws ExecutionException, InterruptedException {
+        RSet<T> rSet = redissonClient.getSet(key);
+        RFuture<Boolean> bb = rSet.addAsync(bean);
+        return bb.get();
     }
 
     public <T> boolean addAll(String key, Set<T> set) {
